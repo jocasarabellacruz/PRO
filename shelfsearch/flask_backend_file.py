47aca837005663c54d2fef80329818c7f50c751b
Adddyy09 <beloadrianmorrissey@gmail.com>
2024-12-20 22:34:39 +0800

    routing home-libris

--- shelfsearch/flask_backend_file.py
+++ shelfsearch/flask_backend_file.py
@@ -1,9 +1,17 @@
-from flask import Flask, jsonify
-from flask_cors import CORS  # Import Flask-CORS
+import os
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
 import subprocess
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+app = Flask(__name__,
+            static_folder=os.path.join(os.getcwd(), 'chatbot/static'),
+            template_folder=os.path.join(os.getcwd(), 'chatbot/templates'))
+
+CORS(app, origins="http://127.0.0.1:5500")  # Allow CORS from the frontend URL
+
+@app.route('/')
+def index():
+    return render_template("chatbot.html")  # Serve your chatbot's HTML template
 
 @app.route('/start-chatbot', methods=['POST'])
 def start_chatbot():
